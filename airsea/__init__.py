62aabc401a29252459f3a6da39aa12cf67f55764
ocefpaf <ocefpaf@gmail.com>
2014-07-21 12:03:41 -0300

    Removed metadata.

--- airsea/__init__.py
+++ airsea/__init__.py
@@ -5,17 +5,10 @@ Python AirSea
 ===============
 """
 
-__authors__ = ["Filipe Fernandes"]
-__copyright__ = ["left"]
-__credits__ = ["Matlab version authors: RP and Phil AA"]
-__license__ = ["MIT"]
-__version__ = ["0.0.1"]
-__maintainer__ = ["Filipe Fernandes"]
-__email__ = ["ocefpaf@gmail.com"]
-__status__ = ["Alpha"]
-__all__ = ["airsea"]
-__obs__ = ["Translated from matlab airsea toolbox Version 2.0"]
-__web__ = ['http://ocefpaf.tiddlyspot.com/']
-__created__ = ["11-26-2010"]
-__modified__ = ["12-01-2010"]
-__all__ = ["atmosphere", "windstress", "constants"]
+from __future__ import absolute_import
+
+__version__ = "0.0.1"
+
+import .constants
+import .atmosphere
+import .windstress
