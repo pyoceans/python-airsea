a2ca9e2011d8b4ef397b59141ff5cf7482ca2982
ocefpaf <ocefpaf@gmail.com>
2014-07-21 12:25:02 -0300

    Performing proper absolute imports.

--- airsea/__init__.py
+++ airsea/__init__.py
@@ -9,6 +9,6 @@ from __future__ import absolute_import
 
 __version__ = "0.0.1"
 
-import .constants
-import .atmosphere
-import .windstress
+from . import constants
+from . import atmosphere
+from . import windstress
